9690296a044f49e2a446334778d43a45f3004b89
Ragini340 <ragini31bxr@gmail.com>
2022-09-20 06:21:53 +0530

    Inital commit

---
+++ FAInsuranceProject/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FAInsuranceProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index()
+        {
+            return "hello";
+        }
+
+    }
+}
+    
